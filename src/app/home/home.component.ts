12b9f70496e5a69aa128325e8c60de625ad74553
Grace Eunbe Kim <eunbegracekim40@gmail.com>
2017-02-17 02:36:18 -0800

    adding responsive div designs on home page

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, HostListener, ElementRef, ViewChild } from '@angular/core';
 
 import { Title } from './title';
 
@@ -9,14 +9,53 @@ import { Title } from './title';
   styleUrls: [ './home.style.css' ],
   templateUrl: './home.template.html'
 })
+
 export class HomeComponent implements OnInit {
   // Set our default values
   public localState = { value: '' };
-  // TypeScript public modifiers
-  constructor(public title: Title) {}
+  public stickyTimelineWidth: number;
+  public stickyConnectWithMeWidth: number;
+  public stickyConnectWithMeBar: boolean;
+
+  public @ViewChild('stickyTimelineContent') stickyTimelineContent: ElementRef;
+  public @ViewChild('stickyConnectWithMeAnchor') stickyConnectWithMe: ElementRef;
+
+  constructor(public title: Title) {
+
+    this.stickyTimelineWidth = 0;
+    this.stickyConnectWithMeWidth = 0;
+    this.stickyConnectWithMeBar = false;
+
+  }
 
   public ngOnInit() {
-    console.log('hello `Home` component');
+
     // this.title.getData().subscribe(data => this.data = data);
+    this.stickyTimelineWidth = this.stickyTimelineContent.nativeElement.clientWidth;
+
+  }
+
+  @HostListener('window:scroll', ['$event'])
+  onWindowScroll(event: Event) {
+
+    this.checkForStickyBars();
+
+  }
+
+  @HostListener('window:resize', ['$event'])
+  onWindowResize(event: Event) {
+
+     this.checkForStickyBars();
+
+  }
+
+  checkForStickyBars() {
+
+    let stickyConnectWidthMeRect: ClientRect = (<HTMLInputElement>this.stickyConnectWithMe.nativeElement).getBoundingClientRect();
+
+    this.stickyConnectWithMeBar = stickyConnectWidthMeRect.top < 470 ? true : false;
+
+    this.stickyConnectWithMeWidth = this.stickyConnectWithMe.nativeElement.clientWidth;
+
   }
 }
